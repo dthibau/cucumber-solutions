3a06b2c8cce9b47c79f8dca175f5233a20ab1d4a
David THIBAU <david.thibau@gmail.com>
2021-06-17 15:48:21 +0200

    Solution 2.2 - Rédaction feature + snippet

---
+++ 2.2/src/test/java/org/formation/StepDefinitions.java
@@ -0,0 +1,68 @@
+package org.formation;
+
+import io.cucumber.java.fr.Alors;
+import io.cucumber.java.fr.Etantdonnéque;
+import io.cucumber.java.fr.Quand;
+
+public class StepDefinitions {
+
+	@Etantdonnéque("{string} a activé le bouton {string}")
+	public void a_activé_le_bouton(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Quand("{string} fournit un email reconnu")
+	public void fournit_un_email_reconnu(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Alors("le système affiche les informations de {string}")
+	public void le_système_affiche_les_informations_de(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+	@Quand("{string} fournit un email non-connu")
+	public void fournit_un_email_non_connu(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Alors("le système propose le formulaire d'inscription")
+	public void le_système_propose_le_formulaire_d_inscription() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+	@Alors("les modes de réception magasin, livraison, livraison_express sont proposés")
+	public void les_modes_de_réception_magasin_livraison_livraison_express_sont_proposés() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+	@Etantdonnéque("{string} a saisi ses informations personelles")
+	public void a_saisi_ses_informations_personelles(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Quand("{string} valide ses informations personelles")
+	public void valide_ses_informations_personelles(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Alors("les modes de réception magasin sont proposés")
+	public void les_modes_de_réception_magasin_sont_proposés() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
---
+++ 2.2/src/test/resources/org/formation/passer_une_commande.feature
@@ -0,0 +1,38 @@
+# language: fr
+
+Fonctionnalité: Passer une commande
+  Lorsque le prospect a terminé sa séléction de produits, il lui reste à passer la commande.
+  Cette fonctionnalité doit être la plus fluide possible. Elle doit permettre de déterminer le mode de réception souhaité et 
+  de valider ses informations personelles. Une fois validé, le client peut passer au règlement.
+  Eric est un client Lillois connu. Tous les modes de livraison lui sont possibles.
+  Valérie est une nouvelle cliente habitante d'une zone non déservie.
+
+# Rule: Identification
+  Scénario: Le système reconnait Eric
+
+  Etant donné que "Eric" a activé le bouton "Passer une commande"
+  Quand "Eric" fournit un email reconnu
+  Alors le système affiche les informations de "Eric"
+
+  Scénario: Le système ne reconnait pas Valérie
+  
+  Etant donné que "Valérie" a activé le bouton "Passer une commande"
+  Quand "Valérie" fournit un email non-connu
+  Alors le système propose le formulaire d'inscription
+  
+ 
+# Rule:  Modes de livraison  
+  Scénario: Les 3 modes de livraison sont possibles pour Eric
+
+  Etant donné que "Eric" a activé le bouton "Passer une commande"
+  Et que "Eric" fournit un email reconnu
+  Quand "Eric" valide ses informations personelles
+  Alors les modes de réception magasin, livraison, livraison_express sont proposés  
+  
+   
+  Scénario: Seul le mode de livraison magasin est possible pour Valérie
+
+  Etant donné que "Valérie" a activé le bouton "Passer une commande"
+  Et que "Valérie" a saisi ses informations personelles
+  Quand "Valérie" valide ses informations personelles
+  Alors les modes de réception magasin sont proposés 
