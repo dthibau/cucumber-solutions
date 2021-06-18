0db20ab4ab1ec8283b2757260755fa06684ea0e4
David THIBAU <david.thibau@gmail.com>
2021-06-18 17:36:48 +0200

    Solutions 3.3

---
+++ 3.3/src/main/java/org/formation/service/SimpleLivraisonService.java
@@ -0,0 +1,50 @@
+package org.formation.service;
+
+import java.util.Optional;
+
+import org.formation.model.Adresse;
+import org.formation.model.Client;
+import org.formation.model.ModeReception;
+
+public class SimpleLivraisonService implements LivraisonServiceIF {
+
+	@Override
+	public Optional<Client> getClientFromEmail(String email) {
+		
+		return Database.getInstance().getClients().stream()
+		    .filter(c -> c.getEmail().equalsIgnoreCase(email)).findFirst();
+		
+	}
+
+	@Override
+	public boolean isPossible(int codePostal, ModeReception modeReception) {
+		
+		if ( modeReception.equals(ModeReception.MAGASIN)) {
+			return true;
+		}
+		if ( modeReception.equals(ModeReception.LIVRAISON)) {
+			return codePostal > 50000 && codePostal < 96000;
+		}
+		if ( modeReception.equals(ModeReception.LIVRAISON_EXPRESS)) {
+			return codePostal > 75000 && codePostal < 76000;
+		}
+		return false;
+	}
+
+	@Override
+	public Client registerNewClient(int codePostal) {
+		Adresse lilleAdresse = new Adresse();
+		lilleAdresse.setCodePostal(codePostal);
+		lilleAdresse.setNumero(11);
+		lilleAdresse.setRue("Rue poincaré");
+		lilleAdresse.setVille("Lille");
+		Client client = new Client();
+		client.setEmail("valerie@knownnot.com");
+		client.setPrenom("Dupont");
+		client.setNom("Valérie");
+		client.setAdresse(lilleAdresse);
+		
+		return client;
+	}
+
+}
